4ce18e2a71834479a498a623a958a0bfd36a59c2
kunsangG <kunsangdorjay6@gmail.com>
2026-06-28 01:09:14 +0530

    Remove dead export

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,5 +2,4 @@ export { default as FeatureCards } from './FeatureCards';
 export { default as HeroGeometric } from './HeroGeometric';
 export { KineticTextReveal } from './KineticTextReveal';
 export { AsciiBackground } from './AsciiBackground';
-export { default as PaperHeroBackground } from './PaperHeroBackground';
 export { DitherGradient } from './DitherGradient';
